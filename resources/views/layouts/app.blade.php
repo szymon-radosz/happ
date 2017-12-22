420777cadb0c3d36fd878a6e31918974398fe9ea
RadoszSzymon <radoszszymon@gmail.com>
2017-12-22 17:40:14 +0100

    basic templates with sass configuration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,27 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{config('app.name', 'hApp')}}</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
     <body>
-       @yield('content')
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/">HabitApp</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </div>
+        </nav>
+
+
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
